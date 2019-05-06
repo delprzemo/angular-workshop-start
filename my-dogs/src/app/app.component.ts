69dfa738c84ad7b1d5e7e11f42a97ecd00bec4bf
delprzemo <delprzemo@gmail.com>
2019-05-06 18:27:25 +0200

    Initial commit

---
+++ my-dogs/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'my-dogs';
+
+  constructor() {
+    let test = 1;
+  }
+}
