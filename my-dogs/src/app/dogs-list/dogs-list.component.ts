b6338968ceec7b12c4b91f0c9bb6565b44d9471f
delprzemo <delprzemo@gmail.com>
2019-05-09 13:33:38 +0200

    exercise 3

--- my-dogs/src/app/dogs-list/dogs-list.component.ts
+++ my-dogs/src/app/dogs-list/dogs-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DogItem } from './models/dog-item';
+import { DogListService } from './services/dog-list.service';
 
 @Component({
   selector: 'app-dogs-list',
   templateUrl: './dogs-list.component.html',
   styleUrls: ['./dogs-list.component.less']
 })
-export class DogsListComponent implements OnInit {
 
-  constructor() { }
+export class DogsListComponent implements OnInit {
+  dogs: DogItem[]
+
+  constructor(public dogListService: DogListService) {
+    this.dogListService.buildDogList();
+  }
 
   ngOnInit() {
   }
-
 }
---
+++ my-dogs/src/app/dogs-list/models/dog-item.ts
@@ -0,0 +1,6 @@
+export class DogItem {
+    constructor(public id: number,
+        public name: string,
+        public description: string = '',
+        public image: string = '') {}
+}
