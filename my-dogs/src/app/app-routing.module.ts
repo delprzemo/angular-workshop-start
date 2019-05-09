fd0b8906defad0f40b4a0aaf91002be8abe1636d
delprzemo <delprzemo@gmail.com>
2019-05-09 12:37:59 +0200

    Exercise 1

--- my-dogs/src/app/app-routing.module.ts
+++ my-dogs/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DogsListComponent } from './dogs-list/dogs-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'dog-list', component: DogsListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ my-dogs/src/app/dogs-list/dogs-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dogs-list',
+  templateUrl: './dogs-list.component.html',
+  styleUrls: ['./dogs-list.component.less']
+})
+export class DogsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
