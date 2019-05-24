7255ddd3278721a6008ab463c122bcc8a617a09a
delprzemo <delprzemo@gmail.com>
2019-05-24 16:13:06 +0200

    Map panel

--- my-dogs/src/app/map/map.component.ts
+++ my-dogs/src/app/map/map.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Marker {
+  lng: number;
+  lat: number;
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  zoom: number;
+  markers: Array<Marker>;
+  lng = 21.011854;
+  lat = 52.231250;
+
+  constructor() {
+    this.markers = [
+      { lng: 21.011854, lat: 52.230250 },
+      { lng: 21.012854, lat: 52.231250 },
+      { lng: 21.009854, lat: 52.230250 }
+    ];
+
+    this.zoom = 9;
+  }
+
+  addMarker() {
+    this.markers.push({ lng: this.lng, lat: this.lat });
+  }
+
+  zoomIn() {
+    this.zoom++;
+  }
+
+  zoomOut() {
+    this.zoom--;
+  }
 
   ngOnInit() {
+
   }
 
 }
